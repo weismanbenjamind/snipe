604954d514f5b09bff0620d3b8b1713995a3acf3
weismanbenjamind <weismanbenjamind@gmail.com>
2026-09-07 11:56:17 -0400

    Module name to list

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,9 @@
 use crate::errors::RunError;
 
-mod list_targets;
+mod list;
 mod shoot;
 
-pub(crate) use list_targets::run_list_targets_cmd;
+pub(crate) use list::run_list_targets_cmd;
 pub(crate) use shoot::run_shoot_cmd;
 
 pub struct SuccessMsg(pub(crate) String);
